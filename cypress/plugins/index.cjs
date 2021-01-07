b74c41254fa0a09bb7ad8fe8dced2bcea6390170
Maria Tome <mariatome95@hotmail.com>
2021-01-07 11:53:37 +0100

    changed plugins/index.js to commonjs

--- cypress/plugins/index.cjs
+++ cypress/plugins/index.cjs
@@ -18,8 +18,8 @@
  */
 const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  allureWriter(on, config)
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
+	allureWriter(on, config)
 	return config
 }
